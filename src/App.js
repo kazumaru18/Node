861dcd34fe63f501b4f46afec76bbc10378d0e4a
kazumaru18 <k.tobe015@gmail.com>
2021-11-30 16:48:37 +0900

    local

--- src/App.js
+++ src/App.js
@@ -6,93 +6,98 @@ import { ResultReason } from 'microsoft-cognitiveservices-speech-sdk';
 
 const speechsdk = require('microsoft-cognitiveservices-speech-sdk')
 
+var displayText = '';
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            displayText: 'INITIALIZED: ready to test speech...'
-        }
+    this.state = {
+      displayText: '準備完了...'
     }
-    
-    async componentDidMount() {
-        // check for valid speech key/region
-        const tokenRes = await getTokenOrRefresh();
-        if (tokenRes.authToken === null) {
-            this.setState({
-                displayText: 'FATAL_ERROR: ' + tokenRes.error
-            });
-        }
+  }
+
+  async componentDidMount() {
+    // check for valid speech key/region
+    const tokenRes = await getTokenOrRefresh();
+    if (tokenRes.authToken === null) {
+      this.setState({
+        displayText: 'FATAL_ERROR: ' + tokenRes.error
+      });
     }
+  }
 
-    async sttFromMic() {
-        const tokenObj = await getTokenOrRefresh();
-        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
-        speechConfig.speechRecognitionLanguage = 'ja-JP';
-        
-        const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
-        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+  async sttFromMic() {
+    const tokenObj = await getTokenOrRefresh();
+    const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+    speechConfig.speechRecognitionLanguage = 'ja-JP';
 
-        this.setState({
-            displayText: 'speak into your microphone...'
-        });
+    const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+    const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
 
-        recognizer.recognizeOnceAsync(result => {
-            let displayText;
-            if (result.reason === ResultReason.RecognizedSpeech) {
-                displayText = `RECOGNIZED: Text=${result.text}`
-            } else {
-                displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
-            }
+    this.setState({
+      displayText: 'マイクに向かって話してください...'
+    });
 
-            this.setState({
-                displayText: displayText
-            });
-        });
-    }
+    recognizer.recognizeOnceAsync(result => {
+      // let displayText;
+      if (result.reason === ResultReason.RecognizedSpeech) {
+        displayText = `${result.text}`
+      } else {
+        displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+      }
 
-    async fileChange(event) {
-        const audioFile = event.target.files[0];
-        console.log(audioFile);
-        const fileInfo = audioFile.name + ` size=${audioFile.size} bytes `;
+      this.setState({
+        displayText: displayText
+      });
+    });
+  }
 
-        this.setState({
-            displayText: fileInfo
-        });
+  async mario() {
+    window.location.href = 'http://127.0.0.1:8000/map/map/?q=' + this.state.displayText;
+  }
 
-        const tokenObj = await getTokenOrRefresh();
-        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
-        speechConfig.speechRecognitionLanguage = 'ja-JP';
+  async fileChange(event) {
+    const audioFile = event.target.files[0];
+    console.log(audioFile);
+    const fileInfo = audioFile.name + ` size=${audioFile.size} bytes `;
 
-        const audioConfig = speechsdk.AudioConfig.fromWavFileInput(audioFile);
-        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+    this.setState({
+      displayText: fileInfo
+    });
 
-        recognizer.recognizeOnceAsync(result => {
-            let displayText;
-            if (result.reason === ResultReason.RecognizedSpeech) {
-                displayText = `RECOGNIZED: Text=${result.text}`
-            } else {
-                displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
-            }
+    const tokenObj = await getTokenOrRefresh();
+    const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+    speechConfig.speechRecognitionLanguage = 'ja-JP';
 
-            this.setState({
-                displayText: fileInfo + displayText
-            });
-        });
-    }
+    const audioConfig = speechsdk.AudioConfig.fromWavFileInput(audioFile);
+    const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
 
-    render() {
-        return (
-            <Container className="app-container">
-                <h1 className="display-4 mb-3">Speech sample app</h1>
+    recognizer.recognizeOnceAsync(result => {
+      let displayText;
+      if (result.reason === ResultReason.RecognizedSpeech) {
+        displayText = `RECOGNIZED: Text=${result.text}`
+      } else {
+        displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+      }
 
-                <div className="row main-container">
-                    <div className="col-6">
-                        <i className="fas fa-microphone fa-lg mr-2" onClick={() => this.sttFromMic()}></i>
-                        Convert speech to text from your mic.
+      this.setState({
+        displayText: fileInfo + displayText
+      });
+    });
+  }
 
-                        <div className="mt-2">
+  render() {
+    return (
+      <Container className="app-container">
+        <h1 className="display-4 mb-3">音声認識</h1>
+
+        <div className="row main-container">
+          <div className="col-6">
+            <i className="fas fa-microphone fa-lg mr-2" onClick={() => this.sttFromMic()}></i>
+            ←マイクをクリックし、話してください。
+
+            {/* <div className="mt-2">
                             <label htmlFor="audio-file"><i className="fas fa-file-audio fa-lg mr-2"></i></label>
                             <input 
                                 type="file" 
@@ -101,13 +106,14 @@ export default class App extends Component {
                                 style={{display: "none"}} 
                             />
                             Convert speech to text from an audio file.
-                        </div>
-                    </div>
-                    <div className="col-6 output-display rounded">
-                        <code>{this.state.displayText}</code>
-                    </div>
-                </div>
-            </Container>
-        );
-    }
+                        </div> */}
+          </div>
+          <div className="col-6 output-display rounded">
+            <code>{this.state.displayText}</code>
+          </div>
+          <button onClick={() => this.mario()}>検索</button>
+        </div>
+      </Container>
+    );
+  }
 }
